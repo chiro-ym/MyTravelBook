9c73119e73eba8b6187f9cdac21cd837405727e6
chihiro-ym <chihiro@example.com>
2024-10-02 00:55:26 +0900

    サインアップ、ホーム画面、ログイン追加

--- MyTravelBook/mytravelbook/app/views.py
+++ MyTravelBook/mytravelbook/app/views.py
@@ -6,3 +6,14 @@ class TopView(View):
     def get(self, request):
         return render(request, "top.html")
     
+class SignupView(View):
+    def get(self, request):
+        return render(request, "signup.html")
+    
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html")
+    
+class HomeView(View):
+    def get(self, request):
+        return render(request, "home.html")   
